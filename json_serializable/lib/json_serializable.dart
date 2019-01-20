ac0283b22b706e61db0da63ff1390f3159d8f7ce
Matthias <matthias.sokolowski@googlemail.com>
2019-01-20 22:06:43 +0100

    Added a public hook, making the generator available for external use

--- json_serializable/lib/json_serializable.dart
+++ json_serializable/lib/json_serializable.dart
@@ -3,4 +3,5 @@
 // BSD-style license that can be found in the LICENSE file.
 
 export 'src/json_literal_generator.dart' show JsonLiteralGenerator;
-export 'src/json_serializable_generator.dart' show JsonSerializableGenerator;
+export 'src/json_serializable_generator.dart'
+    show JsonSerializableGenerator, generateFromExternal;
